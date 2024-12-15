41d73d45637892c9415e5735b1fac39fe3839135
Clayton Jatobá <52887959+cjatoba@users.noreply.github.com>
2024-12-15 02:05:32 -0300

    Remove unused css file (#2)
    * Update App.js
    
    * Update App.js
    
    * Update App.js
    
    * Delete src/App.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
 import { LobbyScreen } from "./pages/Lobby";
 import { RoomPage } from "./pages/Room";
 
 function App() {
   return (
-    <div className="App font-roboto bg-gray-950 w-screen h-screen">
+    <div className="text-center font-roboto bg-gray-950 w-screen h-screen">
       <Routes>
         <Route path="/" element={<LobbyScreen />} />
         <Route path="/room/:roomId" element={<RoomPage />} />
