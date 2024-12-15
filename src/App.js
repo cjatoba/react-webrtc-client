e6e71723725e97cbf408b56604676c11a9689a2e
Clayton Jatobá <52887959+cjatoba@users.noreply.github.com>
2024-12-15 00:44:28 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
+//import "./App.css";
 import { LobbyScreen } from "./pages/Lobby";
 import { RoomPage } from "./pages/Room";
 
